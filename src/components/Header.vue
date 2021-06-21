0c29e9410caa81626fc29e79c351fe066eb63a27
jitho <jithovjoy@gmail.com>
2021-06-21 20:00:51 +0530

    header component

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+        <h2>
+            <label for="">
+                <span class="fas fa-bars"></span>
+            </label>
+            Demo
+        </h2>
+        <div class="user-wrapper">
+            <i class="fas fa-bell"></i>
+             <img src="../../public/images/user.png" width="45px" height="40px" alt="">
+             <div>
+                    <h4>Admin</h4>
+             </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+  name:"Header"
+}
+</script>
+
