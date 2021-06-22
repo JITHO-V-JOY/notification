d9e932de15698eae0592b6ccd150cb6efe08020c
jitho <jithovjoy@gmail.com>
2021-06-22 12:21:23 +0530

    reverting to 018e3ee

---
+++ src/components/notification/AlertComponent.vue
@@ -0,0 +1,15 @@
+<template>
+     <b-card>
+      <b-card-text>
+        {{notification.title}}
+      </b-card-text>
+        <b-button variant="primary">show</b-button>   
+     </b-card>
+</template>
+
+<script>
+export default {
+   name:"AlertComponent",
+   props:['notification']
+}
+</script>
