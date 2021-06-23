29637e55d66f791032c9c882b58625f9785c9a50
jitho <jithovjoy@gmail.com>
2021-06-24 00:03:11 +0530

    basic notification component

---
+++ src/components/notifications/Notification.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="notification" v-bind:style="{display: getDisplay}">
+        <div class="alertHeader">
+            <span>Alerts</span>
+            <i class="fas fa-times" v-on:click="toggleDisplay"></i>
+         </div>
+        <div v-if="getNotificationCount">
+                <AlertComponent v-for="notification in getAllNewNotifications" v-bind:notification="notification" v-bind:key="notification.id"/>
+                <button class="alertbtn" v-on:click="toggleAlert">See alert history</button>
+        </div>
+        <div v-else class="alertHome">
+            <img src="../../../public/images/notification.jpg" height="200px" width="200px" alt="">
+            <p>You don't have any alerts</p>
+            <small>You will see notifications appear here</small>
+            <button class="alertbtn" v-on:click="toggleAlert">See alert history</button>
+        </div>
+    </div>
+</template>
+
+<script>
+import {mapActions, mapGetters} from 'vuex'
+import AlertComponent from './AlertComponent.vue'
+export default {
+   name:"Notification",
+   computed:mapGetters(['getDisplay', 'getAllNewNotifications', 'getNotificationCount']),
+   components:{
+       AlertComponent
+   },
+   methods:{
+       ...mapActions(['toggleDisplay', 'toggleAlert'])
+   }
+  
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,7 +13,7 @@ import { mapGetters } from 'vuex'
 // @ is an alias to /src
 import Header from '../components/Header.vue'
 import Main from '../components/Main.vue'
-import Notification from '../components/Notification.vue'
+import Notification from '../components/notifications/Notification.vue'
 export default {
   name: 'Home',
   computed:{
